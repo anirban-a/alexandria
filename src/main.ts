febd5089bed5e811a0aad7bd722476e5c0a5bab4
Anirban Acharya <acharya.anirban9@gmail.com>
2022-09-21 15:39:13 -0400

    restructuring project

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,31 @@
-import express, {Request,Response,Application, query, response} from 'express';
-import { createClient } from '@supabase/supabase-js';
+import express, { Request, Response, Application, query, response } from 'express';
+
 import dotenv from 'dotenv';
+import userRoutes from './routes/user'
+
 
 dotenv.config()
 
-const CONNECTION_URL:string = process.env.SUPABASE_URL || "";
-const CONNECTION_KEY:string = process.env.SUPABASE_ANON_KEY || "";
-const SERVER_PORT:number = parseInt(process.env.PORT || "8080");
 
-console.log(`Using database URL: ${CONNECTION_URL}`)
-console.log(`Using database KEY ${CONNECTION_KEY}`)
+const SERVER_PORT: number = parseInt(process.env.PORT || "8080");
 
-const supabase = createClient(CONNECTION_URL, CONNECTION_KEY)
 
-class ApplicationMain{
+class ApplicationMain {
     private app;
     private port;
 
-    constructor(port:number){
-        this.app = express();  
+
+    constructor(port: number) {
+        this.app = express();
         this.port = port;
     }
 
-    private async setupDB():Promise<void>{
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            'provider': 'github'
-        })
+    
+
+    run(): void {
         
-    }
-
-    validateID() {
-        return (req:Request<{ id: number}>, res:Response<any>, next:Function)=>{
-            if(isNaN(req.params.id))
-                return res.status(400).send('Invalid ID value');
-            next();
-        }
-    }
-
-    run():void{
-        this.setupDB()
-
-        this.app.get("/:id", this.validateID(), async (req: Request<{ id: number}>, response)=>{
-
-            // select username from Test where id=:id
-            const result = await supabase.from('User')
-            .select('username')
-            .eq('id', req.params.id)
-            .single()
-
-            
-            response.send(result.data)
-        })
-        
-        this.app.listen( this.port, ()=>{
+        this.app.use('/api', userRoutes)
+        this.app.listen(this.port, () => {
             console.log(`Application started on at http://localhost:${this.port}`)
         })
     }
