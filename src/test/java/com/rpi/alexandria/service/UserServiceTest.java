9672bd6bd3a7e98349b4755403a5da20abfd45ce
Anirban Acharya <acharya.anirban9@gmail.com>
2022-10-20 11:30:10 -0400

    Added UT stubs for UserService

--- src/test/java/com/rpi/alexandria/service/UserServiceTest.java
+++ src/test/java/com/rpi/alexandria/service/UserServiceTest.java
@@ -1,21 +1,35 @@
 package com.rpi.alexandria.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rpi.alexandria.model.User;
 import com.rpi.alexandria.repository.UserRepository;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
 
 	@MockBean
 	UserRepository userRepository = Mockito.mock(UserRepository.class);
+
+	ObjectMapper objectMapper = new ObjectMapper();
 
 	@Mock
 	UserService userService;
@@ -26,8 +40,18 @@ public class UserServiceTest {
 	}
 
 	@Test
-	void testContextLoads() {
-		assertTrue(true);
+	void testContextLoads() throws JSONException, IOException {
+
+		User user = objectMapper.readValue(getJson("json/user-001.json"), User.class);
+		assertEquals("johhny125", user.getUsername());
+	}
+
+	private String getJson(String filePath) throws IOException, JSONException {
+		Resource resource = new ClassPathResource(filePath);
+		InputStream is = new FileInputStream(resource.getFile());
+		String jsonTxt = IOUtils.toString(is, StandardCharsets.UTF_8);
+		JSONObject json = new JSONObject(jsonTxt);
+		return json.toString();
 	}
 
 }
