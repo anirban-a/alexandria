6671d592c8a9f4a00f58d9eed5b8af33d1724b0e
Matthew Mosley <matthew.e.l.mosley@gmail.com>
2022-10-22 23:36:18 -0400

    ALEXANDRIA-32 API. connection to gmail still doesn't work

--- src/main/java/com/rpi/alexandria/config/EmailConfig.java
+++ src/main/java/com/rpi/alexandria/config/EmailConfig.java
@@ -6,11 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class EmailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        javaMailSender.setUsername("noreply.alexandriaemail@gmail.com");
+        javaMailSender.setPassword("rwiywnifjxtenpau");
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        properties.setProperty("mail.test-connection","true");
+        return properties;
     }
 }
