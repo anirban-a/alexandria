a518824f8ec6768db98f9e6e079d74b3a648d9a4
Anirban Acharya <acharya.anirban9@gmail.com>
2022-11-23 20:34:43 -0500

    Adding data for exchange details

---
+++ src/main/java/com/rpi/alexandria/dto/ExchangeDTO.java
@@ -0,0 +1,43 @@
+package com.rpi.alexandria.dto;
+
+import com.rpi.alexandria.model.Exchange;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExchangeDTO {
+
+    String id;
+
+    String firstPartyId;
+
+    String otherPartyId;
+
+    String firstPartyBookId;
+
+	String otherPartyBookId;
+
+    BookDTO firstPartyBookDetails;
+
+    BookDTO otherPartyBookDetails;
+
+	Boolean completed = false;
+
+	String initiatorId;
+
+
+	public static ExchangeDTO of(Exchange exchange){
+		ExchangeDTO exchangeDTO = new ExchangeDTO();
+		exchangeDTO.setId(exchange.getId());
+		exchangeDTO.setFirstPartyId(exchange.getFirstPartyId());
+		exchangeDTO.setOtherPartyId(exchange.getOtherPartyId());
+		exchangeDTO.setFirstPartyBookId(exchange.getFirstPartyBookId());
+		exchangeDTO.setOtherPartyBookId(exchange.getOtherPartyBookId());
+		exchangeDTO.setCompleted(exchange.getCompleted());
+		exchangeDTO.setInitiatorId(exchange.getInitiatorId());
+
+		return exchangeDTO;
+	}
+}
